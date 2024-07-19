4b3f88d265deb4ed05728763a933c4be22e5e96e
daniel <starrider346@gmail.com>
2024-07-20 00:56:14 +0100

    tweaks

--- js/scripts.js
+++ js/scripts.js
@@ -1,18 +1,18 @@
 function setCanvasSize() {
-    var canvas = document.getElementById('gradientCanvas');
+    const canvas = document.getElementById('gradientCanvas');
     canvas.width = window.innerWidth;
     canvas.height = window.innerHeight;
 }
 
 function drawGradient(cx, cy, radius) {
-    var canvas = document.getElementById('gradientCanvas');
-    var context = canvas.getContext('2d');
+    const canvas = document.getElementById('gradientCanvas');
+    const context = canvas.getContext('2d');
 
     // Clear the canvas
     context.clearRect(0, 0, canvas.width, canvas.height);
 
     // Define gradient
-    var gradient = context.createRadialGradient(cx, cy, 0, cx, cy, radius);
+    const gradient = context.createRadialGradient(cx, cy, 0, cx, cy, radius);
     gradient.addColorStop(0, '#3e0000'); // Center color
     gradient.addColorStop(1, '#000000'); // Edge color
 
@@ -22,17 +22,18 @@ function drawGradient(cx, cy, radius) {
 }
 
 function updateGradientOnScroll() {
-    var canvas = document.getElementById('gradientCanvas');
-    var radius = Math.min(canvas.width, canvas.height) / 1.5; // Smaller radius
+    const canvas = document.getElementById('gradientCanvas');
+    const radius = Math.min(canvas.width, canvas.height) / 1.5; // Smaller radius
 
     // Calculate the scroll percentage
-    var scrollTop = window.pageYOffset || document.documentElement.scrollTop;
-    var docHeight = document.documentElement.scrollHeight - window.innerHeight;
-    var scrollPercent = scrollTop / docHeight;
+    // noinspection JSDeprecatedSymbols
+    const scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+    const docHeight = document.documentElement.scrollHeight - window.innerHeight;
+    const scrollPercent = scrollTop / docHeight;
 
     // Calculate the vertical position based on scroll percentage
-    var cy = canvas.height * (0.9 - 0.8 * scrollPercent); // 90% at top, 10% at bottom
-    var cx = canvas.width / 3; // Horizontal position in the center
+    const cy = canvas.height * (0.9 - 0.8 * scrollPercent); // 90% at top, 10% at bottom
+    const cx = canvas.width / 3; // Horizontal position in the center
 
     drawGradient(cx, cy, radius);
 }
